d965601f0578c387dc335e4f7fe80d4327f19091
thays-delfino <thays.delfino@sptech.school>
2026-05-20 18:42:30 -0300

    Ajuste do model do qui: Update ou Insert do resultado

--- src/models/resultadoQuizModel.js
+++ src/models/resultadoQuizModel.js
@@ -1,12 +1,28 @@
 var database = require("../database/config");
 
-function salvar(idUsuario, pontuacao, total) {
-    var instrucaoSql = `
-        INSERT INTO resultado_quiz (id_usuario, pontuacao, total) 
-        VALUES ('${idUsuario}', '${pontuacao}', '${total}');
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+function salvar(idUsuario, pontuacao, total, data_quiz) {
+    var sqlVerificar = `SELECT id FROM resultado_quiz WHERE id_usuario = '${idUsuario}';`;
+
+    return database.executar(sqlVerificar).then(function(resultado) {
+
+        if (resultado.length > 0) {
+            var sqlUpdate = `
+                UPDATE resultado_quiz 
+                SET pontuacao = '${pontuacao}', total = '${total}', data_quiz = NOW()
+                WHERE id_usuario = '${idUsuario}';
+            `;
+
+            return database.executar(sqlUpdate);
+
+        } else {
+            var sqlInsert = `
+                INSERT INTO resultado_quiz (id_usuario, pontuacao, total, data_quiz) 
+                VALUES ('${idUsuario}', '${pontuacao}', '${total}', NOW());
+            `;
+
+            return database.executar(sqlInsert);
+        }
+    });
 }
 
 module.exports = {
